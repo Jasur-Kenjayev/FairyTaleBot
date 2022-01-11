754564ec854b61177eab58421977969874a8dcb3
Jasur Kenjayev <94048770+Jasur-Kenjayev@users.noreply.github.com>
2022-01-12 00:05:44 +0500

    Add files via upload

---
+++ keyboards/default/tayorkey.py
@@ -0,0 +1,10 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+menuStart = ReplyKeyboardMarkup(
+    keyboard = [
+        [
+            KeyboardButton(text='👤Tayyorman✅'),
+        ],
+    ],
+    resize_keyboard=True
+)
